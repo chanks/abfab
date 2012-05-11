418674b58f65e3f570f2d96bf7f0f74c2f7b16bd
Chris Hanks <christopher.m.hanks@gmail.com>
2012-05-11 15:41:49 -0700

    Minor variable tweaking.

--- lib/abfab/helpers.rb
+++ lib/abfab/helpers.rb
@@ -2,23 +2,21 @@ require 'digest/md5'
 
 module ABFab
   module Helpers
-    def ab_choose(test_name)
-      result = ABFab.tests[test_name].value_for(abfab_id)
+    def ab_choose(name)
+      result = ABFab.tests[name].value_for(abfab_id)
 
       yield result if block_given?
       result
     end
 
-    def ab_test(test_name, &block)
-      ABFab.tests[test_name].add_participant(abfab_id)
+    def ab_test(name, &block)
+      ABFab.tests[name].add_participant(abfab_id)
 
-      ab_choose(test_name, &block)
+      ab_choose(name, &block)
     end
 
-    def fabulous!(*test_names)
-      test_names.each do |test_name|
-        ABFab.tests[test_name].add_conversion(abfab_id)
-      end
+    def fabulous!(*names)
+      names.each { |name| ABFab.tests[name].add_conversion(abfab_id) }
     end
   end
 end
