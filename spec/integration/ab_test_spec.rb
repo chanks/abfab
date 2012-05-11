0dd5ccbec3479ae221921b63dab75fb7108c90d8
Chris Hanks <christopher.m.hanks@gmail.com>
2012-05-11 13:29:45 -0700

    Consolidate similar helper tests into a shared example set.

--- spec/integration/ab_test_spec.rb
+++ spec/integration/ab_test_spec.rb
@@ -1,70 +1,8 @@
 require 'spec_helper'
 
 describe "The ab_test helper" do
-  include ABFab::Helpers
-
-  Words = %w(eleven cranky ferrets gave my mother rabies)
-
-  before do
-    ABFab.configure do
-      define_test :ab_test_example do
-        possibilities Words
-      end
-    end
-  end
-
-  def abfab_id
-    @id ||= random_id
-  end
-
-  def random_id
-    rand(2**16)
-  end
-
-  context "(like the ab_choose helper)" do
-    it "should return the same results every time for a given user" do
-      results = (1..100).map { ab_test :ab_test_example }
-      results.uniq.length.should == 1
-      Words.should include results.first
-    end
-
-    it "should return different values for different users unpredictably" do
-      results = []
-
-      100.times do
-        results << ab_test(:ab_test_example)
-        @id = nil # New abfab_id each time.
-      end
-
-      results.uniq.sort.should == Words.sort
-      # TODO: Some kind of spec to ensure that the possibilities see a random distribution.
-    end
-
-    it "should yield the value it chooses in a block, as well as returning it" do
-      yielded = nil
-
-      returned = ab_test :ab_test_example do |result|
-        yielded = result
-        "Return value of block doesn't matter."
-      end
-
-      returned.should == yielded
-    end
-
-    it "for different tests should return different values for the same user unpredictably" do
-      # This will fail one in a thousand times. Find a better way to do this?
-
-      ABFab.configure do
-        define_test :hundred_one do
-          possibilities 1000
-        end
-
-        define_test :hundred_two do
-          possibilities 1000
-        end
-      end
-
-      ab_test(:hundred_one).should_not == ab_test(:hundred_two)
-    end
+  context "should behave like an ab_choose helper" do
+    before { @helper_name = :ab_test }
+    it_should_behave_like "an ab_choose helper"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'redis'
 require 'abfab'
 
+# Require shared example groups before running any specs
+Dir["#{File.dirname(__FILE__)}/**/shared/*.rb"].each { |file| require file }
+
 $redis = Redis.new
 
 RSpec.configure do |config|
